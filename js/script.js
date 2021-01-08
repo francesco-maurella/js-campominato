0ded77c1495bd882bc1f7fc4a77b314e0d4e5b73
Francesco Maurella <francesco.maurella@live.it>
2021-01-08 17:48:13 +0100

    Validazione numeri

--- js/script.js
+++ js/script.js
@@ -4,14 +4,44 @@ function getRandomNum(max) {
   return getResult;
 }
 
+// funzione verifica numerica
+function isValid(num, nums, max) {
+  if (isNaN(num)) {
+    alert('Ammessi soltanto caratteri numerici');
+  } if (num > max) {
+    alert('Il numero deve essere compreso tra 1 a 100');
+  } if (nums.includes(num)) {
+    alert('Non puoi inserire lo stesso numero 2 volte');
+  } else {
+    return true
+  }
+}
+
 //dichiarazione array contenitore 16 numeri
 var bombsNum = [];
+var userNums = [101];
+
 
 // ciclo FOR generatore di 16 numeri
 for (var i = 0; i < 16; i++) {
   bombsNum.push(getRandomNum(100))
 }
 
-
 /* verificare se un numero scelto dall'utente rientra nell'array
-attraverso un ciclo WHILE */
+attraverso un ciclo DO WHILE */
+var count = 0;
+var userNum;
+do {
+  userNum = parseInt(prompt('Inserisci un numero'));
+  alert(count)
+  if (!isValid(userNum, userNums, 100)) {
+    count;
+  } else {
+    userNums.push(userNum)
+    count++;
+  }
+} while (!bombsNum.includes(userNum));
+
+// esito
+alert('Il numero ' + userNum + ' è una bomba! Partita terminata.');
+alert('Punteggio: ' + count);
