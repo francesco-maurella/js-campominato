ffca53b692852fba7b3bc1aec7b8caa69ab29818
Francesco Maurella <francesco.maurella@live.it>
2021-01-08 16:12:05 +0100

    Creazione files, strutture, commenti.

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// funzione generatrice numeri random
+
+// ciclo FOR generatore di 16 numeri
+
+//dichiarazione array contenitore 16 numeri
+
+/* verificare se un numero scelto dall'utente rientra nell'array
+attraverso un ciclo WHILE */
