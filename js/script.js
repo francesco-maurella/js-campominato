1da4d3a48670e03589281f1622f9e6dd1653e719
Francesco Maurella <francesco.maurella@live.it>
2021-01-08 18:12:30 +0100

    Conclusione

--- js/script.js
+++ js/script.js
@@ -4,41 +4,42 @@ function getRandomNum(max) {
   return getResult;
 }
 
-// funzione verifica numerica
+// funzione verifica requisiti numerici ammessi
 function isValid(num, nums, max) {
   if (isNaN(num)) {
     alert('Ammessi soltanto caratteri numerici');
-  } if (num > max) {
+  } else if (num > max) {
     alert('Il numero deve essere compreso tra 1 a 100');
-  } if (nums.includes(num)) {
+  } else if (nums.includes(num)) {
     alert('Non puoi inserire lo stesso numero 2 volte');
   } else {
     return true
   }
 }
 
-//dichiarazione array contenitore 16 numeri
-var bombsNum = [];
-var userNums = [];
+var numLimit = 100; // var limite massimo deò valore numerico
+var bombsNum = []; // dichiarazione array contenitore 16 numeri "bomba"
+var userNums = []; // dichiarazione array contenitore numeri utente
 
 
 // ciclo FOR generatore di 16 numeri
 for (var i = 0; i < 16; i++) {
-  bombsNum.push(getRandomNum(100))
+  bombsNum.push(getRandomNum(numLimit))
 }
 console.log(bombsNum)
 
 /* verificare se un numero scelto dall'utente rientra nell'array
 attraverso un ciclo DO WHILE */
-var count = 0;
-var userNum;
+var userNum; // dichiariamo var numero utente
 do {
+  // riempiamo var numero utente
   userNum = parseInt(prompt('Inserisci un numero'));
-  if (isValid(userNum, userNums, 100)) {
+  // verifichiamo requisiti descritti nella funzione
+  if (isValid(userNum, userNums, numLimit)) {
     userNums.push(userNum); // inserisce il numero nel suo array
   }
 } while (!bombsNum.includes(userNum));
 
 // esito
 alert('Il numero ' + userNum + ' è una bomba! Partita terminata.');
-alert('Punteggio: ' + parseInt(userNums.length) + 1);
+alert('Punteggio: ' + parseInt(userNums.length);
