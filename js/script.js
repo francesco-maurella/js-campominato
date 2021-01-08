96e02869a16cc121279e5c149d6bf06711e76503
Francesco Maurella <francesco.maurella@live.it>
2021-01-08 16:29:30 +0100

    Inizio riempimento script

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
 // funzione generatrice numeri random
-
-// ciclo FOR generatore di 16 numeri
+function getRandomNum(max) {
+  getResult = parseInt(Math.floor((Math.random() * max) + 1));
+  return getResult;
+}
 
 //dichiarazione array contenitore 16 numeri
+var bombsNum = [];
+
+// ciclo FOR generatore di 16 numeri
+for (var i = 0; i < 16; i++) {
+  bombsNum.push(getRandomNum(100))
+}
+
 
 /* verificare se un numero scelto dall'utente rientra nell'array
 attraverso un ciclo WHILE */
