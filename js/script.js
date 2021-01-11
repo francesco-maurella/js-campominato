ce7fe38877fafbd7102cefbc963a30f229d5ed89
Francesco Maurella <francesco.maurella@live.it>
2021-01-11 12:58:36 +0100

    Reso funzioni ancora più generiche. FINE

--- js/script.js
+++ js/script.js
@@ -6,28 +6,22 @@ function getRandomNum(max) {
 
 // funzione inserimento di numero, verificandone la validità numerica
 function getNum() {
-    var choice = parseInt(prompt('Inserisci un numero'));
-    if (isNaN(choice)) {
-      alert('Ammessi soltanto caratteri numerici');
-    } else {
-      return choice;
-    }
+  var choice = parseInt(prompt('Inserisci un numero'));
+  if (isNaN(choice)) {
+    alert('Ammessi soltanto caratteri numerici');
+  } else {
+    return choice;
+  }
 }
 
 // funzione verifica limite minimo e massimo di un numero
 function isExtra(num, max) {
-  if (num == 0 || num > max) {
-    alert('Il numero deve essere compreso tra 1 e ' + max);
-    return true;
-  }
+  return (num == 0 || num > max);
 }
 
 // funzione verifica numero duplicato
-function isReply(num, nums) {
-  if (nums.includes(num)) {
-    alert('Numero già inserito precedentemente!');
-    return true;
-  }
+function isDuplicate(num, nums) {
+  return (nums.includes(num));
 }
 
 // funzione equivalenza quantità numeri & capacità contenitore
@@ -40,13 +34,16 @@ alert('CAMPO MINATO\n\nInserisci i numeri, evitando di scrivere quelli "bomba"')
 alert('Per giocare imposta un livello di difficoltà');
 alert('Scrivi:\n\n "1" per Facile.\n "2" per Medio.\n "3" per Difficile.');
 
+var level; // dichiariamo var "livello"
+var levels = 3; // numero livelli
+
 // ciclo DO WHILE per selezionare livello difficoltà
 do {
-  var level = getNum(); // scriviamo un numero valido
-  if (isExtra(level, 3)) {
-    level = false; // se maggiore di 3 il ciclo ripartirà
+  level = getNum(); // scriviamo un numero livello valido
+  if (isExtra(level, levels)) {
+    alert('Il numero deve essere compreso tra 1 e ' + levels);
   }
-} while (!level);
+} while (isExtra(level, levels));
 
 var numsLimit; // dichiarazione var quantità numeri
 
@@ -67,35 +64,44 @@ switch (level) {
 alert('Livello difficoltà: ' + level + '\n' + numsLimit + ' tentativi disponibili');
 alert('Iniziamo');
 
-var bombsNum = []; // dichiarazione array contenitore 16 numeri "bomba"
-var bombsQuantity = 16; // quantità dei numeri bomba
+var bomb; // dichiarazione var "numero boba"
+var bombs = []; // dichiarazione array contenitore 16 numeri "bomba"
 
-// ciclo FOR generatore di 16 numeri
-for (var i = 0; i < bombsQuantity; i++) {
-  bombsNum.push(getRandomNum(numsLimit));
-}
-console.log(bombsNum);
+// ciclo DO WHILE per generare contenuto dei numeri bomba
+do {
+  bomb = getRandomNum(numsLimit); // generiamo un numero bomba
+  if (!isDuplicate(bomb, bombs)) { // se non ci sono numeri duplicati
+    bombs.push(bomb); // il numero viene inserito nel contenitore "numeri bomba"
+  }
+} while (!isEqual(bombs, 16)) // fine ciclo quando la quantità di bombe è = 16
+
+console.log(bombs);
 
 var usrNum; // dichiariamo var numero utente
 var usrNums = []; // dichiarazione array contenitore numeri utente
-var usrNmsCapacity = numsLimit - bombsQuantity; // capacità array numeri utente
+var usrNmsCapacity = numsLimit - bombs.length; // capacità array numeri utente
 var win; // dichiariamo var vittoria
-var loose; // dichiariamo var perdita
+var loose; // dichiariamo var sconfitta
 
 // ciclo DO WHILE per iniziare inserimento numeri utente
 do {
   // scriviamo un numero valido
   usrNum = getNum();
-  // verifichiamone i requisiti (se non è duplicato e se rientra nel quantity-range)
-  if (!isReply(usrNum, usrNums) && !isExtra(usrNum, numsLimit)) {
+  if (isDuplicate(usrNum, usrNums)) {
+    // verifichiamo se non è duplicato
+    alert('Numero già inserito precedentemente!');
+  } else if (isExtra(usrNum, numsLimit)) {
+    // verifichiamo se rientra nel quantity-range
+    alert('Il numero deve essere compreso tra 1 e ' + numsLimit);
+    } else if (usrNum && !isDuplicate(usrNum, bombs)) {
     // inseriamo la scelta nelL'apposito array
     usrNums.push(usrNum);
   }
   // vinciamo quando raggiungiamo capacità max del contenitore usrNums
   win = isEqual(usrNums, usrNmsCapacity);
   // perdiamo quando il numero utente è presente tra i numeri bomba
-  loose = bombsNum.includes(usrNum);
-  // ciclo continua finchè non è vera la "vittoria" o la "perdita"
+  loose = isDuplicate(usrNum, bombs);
+  // ciclo continua finchè non è vera la "vittoria" o la "sconfitta"
 } while (!win && !loose);
 
 /* --- Con l'operatore OR non funziona, non capisco perchè:
@@ -107,7 +113,6 @@ if (win) { // in caso di vittoria
   alert('Complimenti, hai evitato tutti i numeri bomba!');
 } else { // in caso di sconfitta
   alert('Il numero ' + usrNum + ' è una bomba! Partita terminata.');
-  usrNums.length = usrNums.length - 1; // - 1 per non contare il num. bomba
 }
 
 alert('Punteggio: ' + usrNums.length); // punteggio
